889cd72ce347fbe481bb56f41e181103a54a2aa3
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-06-30 14:46:07 +0100

    initial tests passed

--- high_scores/tests/high_scores_test.py
+++ high_scores/tests/high_scores_test.py
@@ -10,9 +10,15 @@ class HighScoresTest(unittest.TestCase):
     # Tests
 
     # Test latest score (the last thing in the list)
-    
+    def test_latest_score(self):
+        scores = [123, 568, 700, 350, 110]
+        self.assertEqual(110, latest(scores))
 
     # Test personal best (the highest score in the list)
+    def test_highest_score(self):
+        scores = [123, 568, 700, 350, 110]
+        self.assertEqual(700, personal_best(scores))
+
 
     # Test top three from list of scores
 
@@ -23,4 +29,3 @@ class HighScoresTest(unittest.TestCase):
     # Test top three when there are less than three
 
     # Test top three when there is only one
-    
