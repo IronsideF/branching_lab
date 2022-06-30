81541f92dd6c3513c4d757a32ea8a77410793f18
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-06-30 14:03:27 +0100

    get_result thoroughly tested

--- football_results/src/football_results.py
+++ football_results/src/football_results.py
@@ -1,5 +1,16 @@
+
+
+
 def get_result(final_score):
-    pass
+    if type(final_score) == dict and "home_result" in list(final_score.keys()) and "away_result" in list(final_score.keys()):
+        if final_score["home_result"] > final_score["away_result"]:
+            return "Home Win"
+        if final_score["home_result"] < final_score["away_result"]:
+            return "Away Win"
+        else:
+            return "Draw"
+    else:
+        return "Invalid input"
 
 def get_results(final_scores):
     pass
--- football_results/tests/football_results_test.py
+++ football_results/tests/football_results_test.py
@@ -2,12 +2,45 @@ import unittest
 from src.football_results import *
 
 class FootballResultsTest(unittest.TestCase):
-    pass
-    # Test we get the right result string for a final score dictionary representing -
-
-        # Home win
-        # Away win
-        # Draw
+    
+    
+        # away_win = {
+        # "home_result": 0,
+        # "away_result": 1
+        # }
+        # draw = {
+        # "home_result": 1,
+        # "away_result": 1
+        # }
+    def test_result_home_win(self):
+        home_win = {
+        "home_result": 1,
+        "away_result": 0
+        }
+        self.assertEqual("Home Win", get_result(home_win))
+    def test_result_away_win(self):
+        away_win = {
+        "home_result": 0,
+        "away_result": 1
+        }
+        self.assertEqual("Away Win", get_result(away_win))
+    def test_result_draw(self):
+        draw = {
+        "home_result": 1,
+        "away_result": 1
+        }
+        self.assertEqual("Draw", get_result(draw))
+    
+    def test_result_invalid(self):
+        invalid = "We won"
+        self.assertEqual("Invalid input", get_result(invalid))
+    
+    def test_result_bad_dict(self):
+        bad_input = {
+        "man_u_result": 3,
+        "liverpool": 2
+        }
+        self.assertEqual("Invalid input", get_result(bad_input))
 
     # Test we get right list of result strings for a list of final score dictionaries. 
 
